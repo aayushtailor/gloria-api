9a375fcb9ef42aeb8354f8d4bbbef0ca08c72314
Aayush Tailor <aayushtailor16@gmail.com>
2025-10-03 14:27:04 +0530

    Update Product.js

--- models/Product.js
+++ models/Product.js
@@ -1,14 +1,14 @@
-import mongoose from "mongoose";
+const mongoose = require("mongoose");
 
-const productSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    price: { type: Number, required: true },
-    stock: { type: Number, default: 0 },
-    description: String,
-    imageUrl: String,
-  },
-  { timestamps: true }
-);
+const productSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  price: { type: Number, required: true },
+  description: { type: String },
+  image: { type: String },
+  gallery: [String],
+  category: { type: String },
+  rating: { type: Number, default: 4 }
+}, { timestamps: true });
 
-export default mongoose.model("Product", productSchema);
+const Product = mongoose.model("Product", productSchema);
+module.exports = Product;
